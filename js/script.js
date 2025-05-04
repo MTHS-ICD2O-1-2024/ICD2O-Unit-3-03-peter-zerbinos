ab0da8152b51b805daacab40886ffe074a69c7e8
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-04 23:14:30 +0000

    index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateVolumeOfSphere() {
+  const radiusOfSphere = parseFloat(document.getElementById('radius-of-sphere').value)
+
+  const volumeOfSphere = (4 / 3) * (Math.PI) * (radiusOfSphere ** 3)
+
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volumeOfSphere.toFixed(2) + 'mm³'
+}
